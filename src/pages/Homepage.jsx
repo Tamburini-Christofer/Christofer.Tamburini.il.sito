73f12355ae27d59354b8457335825cf9ab39755f
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-03-03 19:36:51 +0100

    Creazione bozza Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,19 @@
+//import 
+import { NavLink } from "react-router-dom";
+//import 
+
 export default function Homepage () {
     return (
         <>
-        
+            <div id="containerHome">
+                <div className="containerNavBar">
+                    <div className="contInfo"></div>
+                    <NavLink to="/webDeveloper"><div className="containerPages webDeveloper"></div></NavLink>
+                    <NavLink to="/photography"><div className="containerPages photography"></div></NavLink>
+                    <NavLink to="/blender"><div className="containerPages blender"></div></NavLink>
+                    <NavLink to="/stampa3D"><div className="containerPages stampa3D"></div></NavLink>
+                </div>
+            </div>
         </>
     )
 }
