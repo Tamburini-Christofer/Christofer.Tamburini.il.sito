109ee8313666d877e33d22a93015f5c8de9267f6
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-04-06 14:53:17 +0200

    Creazione Hero Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,19 @@
-export default function Homepage () {
-    return(
-        <>
-        </>
-    )
+export default function Homepage() {
+  return (
+    <>
+      <div className="contenitoreHomepage">
+        <div className="titleHome">
+          <h5>PORTFOLIO PERSONALE</h5>
+          <h1>
+            Creo esperienze <br />
+            <strong>digitali e fisiche</strong>
+          </h1>
+          <p>
+            Web development, fotografia, video aereo e progettazione 3D quattro
+            discipline, un'unica visione creativa.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
