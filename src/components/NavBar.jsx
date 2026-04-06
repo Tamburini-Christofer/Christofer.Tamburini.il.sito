d57a1773f89da95fedd600d1b87be316efd41d29
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-04-06 14:05:28 +0200

    Creazione componente NAvBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,31 @@
-export default function NavBar () {
-    return(
-        <>
-        </>
-    )
+import { NavLink } from "react-router-dom";
 
+let NavBarMenu = [
+  { route: "/", label: "Homepage" },
+  { route: "web-developer", label: "Web Developer" },
+  { route: "fotografia", label: "Fotografia" },
+  { route: "video-droni", label: "Video & Drone" },
+  { route: "Stampa3D", label: "Stampoa3D" },
+];
+
+export default function NavBar() {
+  return (
+    <>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <ul>
+            {NavBarMenu.map((link, index) => {
+              <li key={index}>
+                <NavLink
+                  to={link.route}>
+                  {link.label}
+                </NavLink>
+              </li>;
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
